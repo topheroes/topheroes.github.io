c6a9a9a02cb71614bf2e529e7af4b54108ab3ec1
Pavel <tr@tr.tr>
2019-01-29 18:26:40 +0300

    not compressed js files

--- js/preloader.js
+++ js/preloader.js
@@ -1,1 +1,53 @@
-function done(){var e=document.getElementById("ppreloader");e.classList.contains("done")||e.classList.add("done")}setTimeout(function(){for(var e=document.images,n=e.length,o=0,t=0;t<n;t++){var a=new Image;a.src=e[t].src,a.onload=d,a.onerror=d}function d(){++o>=n&&done()}},1e3);
+
+
+ setTimeout(function(){ 
+
+	var images = document.images,
+		images_length = images.length,
+		images_loaded = 0;
+
+
+
+	for(var i = 0; i < images_length; i++){
+
+		var cur_img = new Image();
+		cur_img.src = images[i].src;
+		cur_img.onload = oneLoaded;
+		cur_img.onerror = oneLoaded;
+
+	}
+
+	
+
+	function oneLoaded(){
+
+		
+
+		if( (++images_loaded) >= images_length){
+			done();
+		}
+
+
+	}
+
+
+}, 1000);
+
+
+
+
+ function done(){
+
+
+	
+
+		var preloader = document.getElementById("ppreloader");
+		if( !preloader.classList.contains("done"))
+		{
+
+			preloader.classList.add("done");
+
+		}
+	
+
+}
